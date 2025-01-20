f803d005e1bd8c6f967847bd4df6dd56fbe7f609
정용석 <jysdydtjr@synapsoft.co.kr>
2025-01-20 16:48:27 +0900

    [PGS] JadenCase 문자열 만들기 / Lv.2

---
+++ yong/Lv.2_JadenCase_문자열_만들기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    bool temp = true;
+    for(int i = 0; i < s.size(); i++){
+        if(temp && s[i] >= 'a' && s[i] <= 'z'){
+            temp = false;
+            s[i] = s[i] + 'A' - 'a'; // toupper() 함수를 사용하는 방법도 있다.
+        }
+        else if(!temp && s[i] >= 'A' && s[i] <= 'Z'){
+            s[i] = s[i] - 'A' + 'a'; // tolower() 함수를 사용하는 방법도 있다.
+        }
+        if(s[i] == ' '){
+            temp = true;
+        }else{
+            temp = false;
+        }
+    }
+    answer = s;
+    return answer;
+}
