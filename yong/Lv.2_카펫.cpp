2fb2d729f9170414410ffb372fb9d2dba9c8e5ca
Yongseok Jeong <jysdydtjr@gmail.com>
2025-01-27 19:19:19 +0900

    [PGS] 카펫 ' Lv.2

---
+++ yong/Lv.2_카펫.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer(2);
+    
+    int area = brown + yellow;
+    for(int i = 1; i < area; i++){
+        int j = area / i;
+        if(2*i+2*j-4 == brown && i * j == area){
+            answer[0] = j;
+            answer[1] = i;
+            break;
+        }
+    }
+    
+    return answer;
+}
