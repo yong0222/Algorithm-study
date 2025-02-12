e97faeacf835bf23de9d4de72062fa8710dedb94
정용석 <jysdydtjr@synapsoft.co.kr>
2025-02-12 09:17:46 +0900

    [PGS] H-Index / Lv.2

---
+++ yong/Lv.2_H-Index.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    
+    sort(citations.rbegin(), citations.rend());
+    
+    for(int i = 0; i < citations.size(); i++){
+        if(citations[i] <= i){
+            answer = i;
+            break;
+        }
+        if(i+1 == citations.size()){
+            answer = i + 1;
+        }
+    }
+    
+    return answer;
+}
